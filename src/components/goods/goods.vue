b1b712d27a06771908a85543b927c8c2bb5f9487
John-PC\John <18702516203@126.com>
2017-05-24 17:01:28 +0800

    完成右侧食品列表的联动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,15 +2,15 @@
   <div class="goods">
     <div class="meau-wrapper" ref="meauWrapper">
       <ul>
-        <li v-for="item in goods" class="meau-item">
+        <li v-for="(item,index) in goods" class="meau-item" :class="{'current':currentIndex === index}" @click="selectMeau(index,$event)">
           <span class="text border-1px">
-          	<span v-show="item.type>0" :class="classMap[item.type]" class="icon"></span>{{item.name}}</span>
+            <span v-show="item.type>0" :class="classMap[item.type]" class="icon"></span>{{item.name}}</span>
         </li>
       </ul>
     </div>
     <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -44,8 +44,24 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      srcollY: 0,
+      foodsScroll: null
     };
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let h1 = this.listHeight[i];
+        let h2 = this.listHeight[i + 1];
+        // 滚动的位置在某一区块时
+        if (!h2 || (this.srcollY >= h1 && this.srcollY < h2)) {
+          return i;
+        }
+      }
+      return 0;
+    }
   },
   created() {
     this.$http.get('/api/goods').then(response => {
@@ -54,6 +70,7 @@ export default {
         this.goods = response.data;
         this.$nextTick(() => {
           this.initScroll();
+          this.caculateHeight();
         });
       }
     });
@@ -61,9 +78,39 @@ export default {
   },
   methods: {
     initScroll() {
-      this.menuScroll = new BScroll(this.$refs.meauWrapper, {});
-
-      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+      // click: true恢复磨人的点击事件
+      this.menuScroll = new BScroll(this.$refs.meauWrapper, {
+        click: true
+      });
+      // probeType: 3 监测实时滚动的位置
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      });
+      this.foodsScroll.on('scroll', (pos) => {
+        // 获取滚动的位置的纵坐标(正值)
+        this.srcollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    caculateHeight() {
+      // 获取每个区块的高度
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    selectMeau(index, event) {
+      // 去掉pc端自带的点击事件
+      if (!event._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      // 滚动到相应的节点
+      let pos = foodList[index];
+      this.foodsScroll.scrollToElement(pos, 300);
     }
   }
 };
